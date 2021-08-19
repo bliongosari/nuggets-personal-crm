9efda05711eceec817e4fdb93677fb0d0a6d7f3f
Brandon Vincent <brandonvincent567@gmail.com>
2021-08-20 03:11:47 +1000

    added nodemon

--- server/app.js
+++ server/app.js
@@ -12,13 +12,17 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/", routes);
+//app.use("/", routes);
 
 PORT = process.env.PORT || 3001;
 
 // if (process.env.NODE_ENV == "production") {
 //   app.use(express.static("./client/build"));
 // }
+
+app.all("*", (req, res) => {
+  res.send("Hi, this is the backend");
+});
 
 app.listen(PORT, () => console.log("Backend on port " + PORT));
 
