b8f4f5c37aec0e9a2b6107bbd7296e9daecd0674
csoeryawinat <70050492+csoeryawinat@users.noreply.github.com>
2021-08-20 10:31:21 +0700

    Added models

---
+++ server/models/event.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  event_name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: [Number],
+  },
+  type: {
+    type: String,
+  },
+  start_time: {
+    type: Date,
+    required: true,
+  },
+  end_time: {
+    type: Date,
+    required: true,
+  },
+  repeat: {
+    type: String,
+    enum: ['Never','Every Day','Every Week','Every Fortnight','Every Month','Every Year'],
+    default: 'Never',
+  },
+  end_repeat: {
+      type: Date,
+  },
+  alert: {
+    type: String,
+    enum: ['None','At time of event', '5 minutes before','10 minutes before','15 minutes before',
+            '20 minutes before', '1 hour before','2 hours before','1 day before','2 days before',
+            '1 week before'],
+    defailt: 'None',
+  },
+  notes: {
+      type: String,
+  }
+});
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
