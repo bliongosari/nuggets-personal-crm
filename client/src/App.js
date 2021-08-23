21caaf4a542954c7cb7c189d725b20e236700037
chiquittakellie <ckellie@student.unimelb.edu.au>
2021-08-23 15:11:59 +0700

    added frontend pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Welcome from "./components/Welcome/Welcome.js"
 import Home from "./components/Home/Home.js";
 import Login from "./components/Login/Login.js";
+import Signup from "./components/Signup/Signup.js"
+import Sidebar from "./components/Sidebar/Sidebar.js"
 import Navbar from "./components/Navbar/Navbar.js";
 
 function App() {
@@ -11,8 +14,10 @@ function App() {
       <div className="app">
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Welcome} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/home" component={Home} />
         </Switch>
       </div>
     </BrowserRouter>
