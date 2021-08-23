2bd9c965411bbab1306a71223600b2a3018cd30f
chiquittakellie <ckellie@student.unimelb.edu.au>
2021-08-23 15:35:47 +0700

    added frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 import Welcome from "./components/Welcome/Welcome.js"
 import Home from "./components/Home/Home.js";
 import Navbar from "./components/Navbar/Navbar.js";
+import UserProfile from "./components/UserProfile/UserProfile";
+import Journal from "./components/Journal/Journal";
+import Contacts from "./components/Contacts/Contacts";
+import Events from "./components/Events/Events";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={Welcome} />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/user-profile" component={UserProfile} />
+          <Route exact path="/journal" component={Journal} />
+          <Route exact path="/contacts" component={Contacts} />
+          <Route exact path="/events" component={Events} />
 
         </Switch>
       </div>
---
+++ client/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./UserProfile.css";
+
+function UserProfile() {
+  return (
+    <div className="userprofile">
+      <h1>This is User Profile page</h1>
+    </div>
+  );
+}
+
+export default UserProfile;
