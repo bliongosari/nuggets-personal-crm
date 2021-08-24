825a20c4684e6cfa59fca4d625f42c8766d50f22
chiquittakellie <ckellie@student.unimelb.edu.au>
2021-08-24 11:05:38 +0700

    fixed bugs

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -5,19 +5,19 @@ function Sidebar() {
   return (
     <div>
       <div className="sidebar">
-        <div class="sidebar-container">
+        <div className="sidebar-container">
           <a href="/home"><img alt="" src="../../home.svg"></img><span>Home</span></a>
         </div>
-        <div class="sidebar-container">
+        <div className="sidebar-container">
           <a href="/journal"><img alt="" src="../../journal.svg"></img><span>Journal</span></a>
         </div>
-        <div class="sidebar-container">
+        <div className="sidebar-container">
           <a href="/events"><img alt="" src="../../events.svg"></img><span>Events</span></a>
         </div>
-        <div class="sidebar-container">
+        <div className="sidebar-container">
           <a href="/contacts"><img alt="" src="../../contacts.svg"></img><span>Contacts</span></a>
         </div>
-        <div class="sidebar-container">
+        <div className="sidebar-container">
           <a href="/"><img alt="" src="../../logout.svg"></img><span>Log out</span></a>    
         </div>
       </div>
--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -3,30 +3,30 @@ import "./UserProfile.css";
 
 function UserProfile() {
   return (
-    <div class="user-profile">
-      <div class="background">
-        <img alt="User" src="../../person.svg" class="user-image"></img>
-        <h1 class="user-full-name">User Full Name</h1>
+    <div className="user-profile">
+      <div className="background">
+        <img alt="User" src="../../person.svg" className="user-image"></img>
+        <h1 className="user-full-name">User Full Name</h1>
       </div>
 
-      <div class="grid-container">
-        <h1 class="user-data-spec">First Name:</h1>
-        <h1 class="user-data">(first name)</h1>
-        <h1 class="user-data-spec">Last Name:</h1>
-        <h1 class="user-data">(last name)</h1>
-        <h1 class="user-data-spec">Email Address:</h1>
-        <h1 class="user-data">(email address)</h1>
+      <div className="grid-container">
+        <h1 className="user-data-spec">First Name:</h1>
+        <h1 className="user-data">(first name)</h1>
+        <h1 className="user-data-spec">Last Name:</h1>
+        <h1 className="user-data">(last name)</h1>
+        <h1 className="user-data-spec">Email Address:</h1>
+        <h1 className="user-data">(email address)</h1>
       </div>
 
-      <div class="button-container">
-        <button class="button">
-          <img alt="Edit" src="../../edit.svg" class="icon"></img>Edit Profile
+      <div className="button-container">
+        <button className="button">
+          <img alt="Edit" src="../../edit.svg" className="icon"></img>Edit Profile
         </button>
       </div>
 
-      <div class="button-container">
-        <button class="button1">
-          <img alt="Edit" src="../../edit.svg" class="icon"></img>Change Password
+      <div className="button-container">
+        <button className="button1">
+          <img alt="Edit" src="../../edit.svg" className="icon"></img>Change Password
         </button>
       </div>
       
