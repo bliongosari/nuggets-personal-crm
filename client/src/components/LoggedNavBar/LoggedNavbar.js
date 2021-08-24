522bf0b49ae003cafa916641e641654300a66c75
eputri29 <eputri@student.unimelb.edu.au>
2021-08-24 19:54:48 +1000

    add icons to navbar

--- client/src/components/LoggedNavBar/LoggedNavbar.js
+++ client/src/components/LoggedNavBar/LoggedNavbar.js
@@ -1,18 +1,30 @@
 import React from "react";
-import "./LoggedNavbar.css";
+import "./LoggedNavBar.css";
 
 
-function Navbar() {
+function LoggedNavbar() {
   return (
     <div className="navbar">
-        <h1>NUGGETS</h1>
+        <div className="burger">
+            <img alt="" src="../../whiteburger.svg"></img>
+        </div>
+        
+        <div className="appname">
+            <h1>NUGGETS</h1>
+        </div>
+        
 
-        <SignInModal/>
-        <SignUpModal/>
+        <div className="notif">
+            <img alt="" src="../../whitenotif.svg"></img>
+        </div>
+
+        <div className="profile">
+            <img alt="" src="../../whiteprofile.svg"></img>
+        </div>
         
         
       </div>
   );
 }
 
-export default Navbar;
+export default LoggedNavbar;
