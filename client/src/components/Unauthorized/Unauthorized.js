5d60e2e02997e6e6807c205678c0d51e3d0950bd
chiquittakellie <ckellie@student.unimelb.edu.au>
2021-08-28 21:33:49 +0700

    added frontent for unauthorized page

--- client/src/components/Unauthorized/Unauthorized.js
+++ client/src/components/Unauthorized/Unauthorized.js
@@ -4,12 +4,23 @@ import "./Unauthorized.css";
 
 function Unauthorized() {
   return (
-    <div className="unauthorized">
-      <h1> Sorry you are unauthorized to view this page </h1>
-      <h1>Please create an account to access this.</h1>
-      <Link to="/">
-        <h1 className="links"> Go to Home</h1>
-      </Link>
+    <div>
+      <div className="nav">
+        <img alt="logo" src="../../logo.svg" className="logo-welcome"></img>
+        <h1 className="title-welcome">NUGGETS</h1>
+      </div>
+      <div className="unauthorized">
+        <div className="sorry">
+          <h1 className="text"> Sorry</h1>
+          <img alt="sorry" src="../../sorry.svg" className="logo-welcome"></img>
+        </div>
+        <h1>You are unauthorized to view this page. </h1>
+        <h1>Please log in to access.</h1>
+        <Link to="/">
+          <h1 className="links"> Go to Home</h1>
+        </Link>
+      </div>
+      
     </div>
   );
 }
