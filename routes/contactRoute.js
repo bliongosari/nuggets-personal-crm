4988006b77b443d96449993886259d7b8ba2f10d
daniel <daniel.barhoum@student.unimelb.edu.au>
2021-09-01 08:51:33 +1000

    Fixed contacts get route

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -3,35 +3,8 @@ const router = express.Router();
 const Contact = require("../models/contact");
 const auth = require("../middleware/auth");
 
-// router.get("/all", auth.authenticateToken, async (req, res) => {
-//   try {
-//     const contacts = await Contact.find({ user_id: req.user.id });
-//     return res
-//       .status(200)
-//       .json({ contacts: contacts, message: "Successfully retrieved" });
-//   } catch (e) {
-//     return res.status(401).json({ message: "No user" });
-//   }
-// });
-
-// router.post("/add", auth.authenticateToken, async (req, res) => {
-//   try {
-//     const newContact = new Contact({
-//       user_id: req.user.id,
-//       full_name: req.body.full_name,
-//       preferred_name: req.body.preferred_name,
-//       birthday: req.body.birthday,
-//     });
-//     await newContact.save();
-//     return res.status(200).json({ message: "Successfully added" });
-//   } catch (e) {
-//     console.log(e);
-//     return res.status(403).json({ message: "Failed to add user. Try again." });
-//   }
-// });
-
-// show contacts
-router.get("/", auth.authenticateToken, async (req, res) => {
+// get contacts
+router.get("/all", auth.authenticateToken, async (req, res) => {
   try {
     const contacts = await Contact.find({ user_id: req.user.id });
     return res
@@ -42,7 +15,7 @@ router.get("/", auth.authenticateToken, async (req, res) => {
   }
 });
 
-// show contact profile
+// get contact profile
 router.get("/:id", auth.authenticateToken, async (req, res) => {
   const contact = Contact.findById(req.params.id);
   contact
