91c4841a634941fae342b72868d3a49b58c7e3e1
daniel <daniel.barhoum@student.unimelb.edu.au>
2021-08-31 14:18:01 +1000

    Made welcome page persist

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,12 +1,11 @@
+import "./Welcome.css";
 import api from "../../config/axiosConfig";
 import React from "react";
 import { useQuery } from "react-query";
-import "./Welcome.css";
 import { useHistory } from "react-router-dom";
 
-
 function Welcome() {
-  const { isError, isSuccess } = useQuery("auth", async () => {
+  const { isSuccess } = useQuery("auth", async () => {
     const { data } = await api.get("/api/user/verifyToken");
     return data;
   }, { staleTime: Infinity });
@@ -14,12 +13,10 @@ function Welcome() {
   if (isSuccess) history.push("/home");
   return (
     <>
-      {isError &&
-        <div className="main-content">
-          <h1>Welcome to Nuggets!</h1>
-          <h2>Your personal organiser, contacts storage, and reminder</h2>
-        </div>
-      }
+      <div className="main-content">
+        <h1>Welcome to Nuggets!</h1>
+        <h2>Your personal organiser, contacts storage, and reminder</h2>
+      </div>
     </>
   );
 }
