b00001e1527ca07f154868ad6adbda7919e6659d
Brandon Vincent <brandonvincent567@gmail.com>
2021-08-31 17:26:16 +1000

    fixed duplicate routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,11 @@ const router = express.Router();
 
 const userAuth = require("./userAuth");
 const contacts = require("./contacts");
-const contactRoute = require("./contactRoute");
 const journalRoute = require("./journalRoute");
 const eventRoute = require("./eventRoute");
 
 router.use("/contacts", contacts);
 router.use("/user", userAuth);
-router.use("/contacts", contactRoute);
 router.use("/journal", journalRoute);
 router.use("/events", eventRoute);
 
