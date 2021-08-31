e3f1e6b98fcc51e0cbf4b4823f6f11017f7e4314
Brandon Vincent <brandonvincent567@gmail.com>
2021-08-31 17:11:28 +1000

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,15 @@ const router = express.Router();
 
 const userAuth = require("./userAuth");
 const contacts = require("./contacts");
+const contactRoute = require("./contactRoute");
+const journalRoute = require("./journalRoute");
+const eventRoute = require("./eventRoute");
 
 router.use("/contacts", contacts);
 router.use("/user", userAuth);
+router.use("/contacts", contactRoute);
+router.use("/journal", journalRoute);
+router.use("/events", eventRoute);
 
 router.get("/", (req, res) => {
   return res.send("api page");
--- routes/journalRoute.js
+++ routes/journalRoute.js
@@ -6,53 +6,60 @@ const auth = require("../middleware/auth");
 
 // show journals
 router.get("/", auth.authenticateToken, async (req, res) => {
-    try {
-        const journals = await Journal.find({ user_id: req.user.id });
-        return res
-          .status(200)
-          .json({ journals: journals, message: "Successfully retrieved" });
-    } catch (e) {
-        return res.status(401).json({ message: "No user" });
-    }
+  try {
+    const journals = await Journal.find({ user_id: req.user.id });
+    return res
+      .status(200)
+      .json({ journals: journals, message: "Successfully retrieved" });
+  } catch (e) {
+    return res.status(401).json({ message: "No user" });
+  }
 });
 
 // create a new journal
 router.post("/create", auth.authenticateToken, async (req, res) => {
-    try{
-        const journal = new Journal({
-        title: req.title,
-        description: req. description,
-        files: req.files,
+  try {
+    const journal = new Journal({
+      title: req.title,
+      description: req.description,
+      files: req.files,
     });
     await journal.save();
     return res.status(200).json({ message: "Successfully added" });
-    } catch (e) {
-      console.log(e);
-      return res.status(403).json({ message: "Failed to add journal. Try again." });
-    }
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(403)
+      .json({ message: "Failed to add journal. Try again." });
+  }
 });
 
 //delete journal
-router.get("/delete/:id", auth.authenticateToken, async (req,res) => {
-    try {
-        const journal = await Journal.findOneAndDelete({_id: req.params.id })
-        .exec();
-        return res.status(200).json({ message: "Successfully deleted" });
-    } catch (e) {
-        console.log(e);
-        return res.status(403).json({ message: "Failed to delete journal. Try again." });
-    }
+router.get("/delete/:id", auth.authenticateToken, async (req, res) => {
+  try {
+    const journal = await Journal.findOneAndDelete({
+      _id: req.params.id,
+    }).exec();
+    return res.status(200).json({ message: "Successfully deleted" });
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(403)
+      .json({ message: "Failed to delete journal. Try again." });
+  }
 });
 
 //edit journal
-router.post("/edit/:id", auth.authenticateToken, async(req,res) => {
-    try { 
-        const journal = Journal.findById(req.params.id);
-        return res.status(200).json({ message: "Successfully edited" });
-    } catch (e) {
-        console.log(e);
-        return res.status(403).json({ message: "Failed to edit journal. Try again." });
-    }
+router.post("/edit/:id", auth.authenticateToken, async (req, res) => {
+  try {
+    const journal = Journal.findById(req.params.id);
+    return res.status(200).json({ message: "Successfully edited" });
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(403)
+      .json({ message: "Failed to edit journal. Try again." });
+  }
 });
 
 module.exports = router;
