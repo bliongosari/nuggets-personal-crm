a5d32938e389793a7c4f62e2aeab54987af87e0f
Brandon Vincent <brandonvincent567@gmail.com>
2021-09-04 02:41:39 +1000

    Added integration (viewing and deleting) of events

--- client/src/components/Events/EventDetail.js
+++ client/src/components/Events/EventDetail.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../config/axiosConfig.js";
 
 function EventDetail(props) {
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  useEffect(() => {
+    console.log(props.event);
+    setStart(new Date(props.event.start).toLocaleDateString("en-US"));
+    setEnd(new Date(props.event.end).toLocaleDateString("en-US"));
+  }, []);
+
+  const deleteEvent = async () => {
+    api({
+      method: "GET",
+      url: "/api/events/delete/" + props.event._id,
+    })
+      .then(function (res) {
+        if (res.status === 200) {
+          window.location.reload(false);
+          alert("Sucessfully Deleted");
+        } else {
+          alert("Failed to Delete");
+        }
+      })
+      .catch(function (error) {
+        alert("Failed to Delete");
+      });
+  };
+
   return (
     <div>
-      <h3> ID: {props.event.id}</h3>
       <h3> Event Name: {props.event.title}</h3>
+      <h3> Alert: {props.event.alert}</h3>
+      <h3> Date start: {start} </h3>
+      <h3> Date end: {end} </h3>
+      <h3> Repeat: {props.event.repeat} </h3>
+      <h3> Repeat: {props.event.alert} </h3>
+      <button onClick={deleteEvent}> Delete Event </button>
     </div>
   );
 }
--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -6,138 +6,126 @@ const router = express.Router();
 
 // show events
 router.get("/", auth.authenticateToken, async (req, res) => {
-    
-    try {
-        const repeat_list = Event.schema.path("repeat").enumValues;
-        const alert_list = Event.schema.path("alert").enumValues;
-        const events = await Event.find({ user_id: req.user.id });
-        return res
-          .status(200)
-          .json({ 
-            user: req.user,
-            repeat_list: repeat_list,
-            alert_list: alert_list,
-            events: events,
-            message: "Successfully retrieved" });
-    } catch (e) {
-        return res.status(401).json({ message: "No user" });
-    }
+  try {
+    const events = await Event.find({ user_id: req.user.id });
+    return res.status(200).json({
+      events: events,
+      message: "Successfully retrieved",
+    });
+  } catch (e) {
+    return res.status(401).json({ message: "No user" });
+  }
 });
 
 // create a new event
 router.post("/create", auth.authenticateToken, async (req, res) => {
-    try {
-        const repeat_list = Event.schema.path("repeat").enumValues;
-        const alert_list = Event.schema.path("alert").enumValues;
-        var repeat = req.body.repeat;
-        var alert = req.body.alert;
-        if(repeat_list.includes(repeat)) {
-            repeat = repeat_list[repeat];
-        }
-        else {
-            repeat = repeat_list[0];
-        }
-        if(alert_list.includes(alert)) {
-            alert= alert_list[alert];
-        }
-        else {
-            alert = alert_list[0];
-        }
-        
-        const event = new Event({
-            user_id: req.body.user_id,
-            event_name: req.body.event_name,
-            location: req.body.location,
-            type: req.body.type,
-            start_time: req.body.start_time,
-            end_time: req.body.end_time,
-            repeat: repeat,
-            alert: alert,
-            notes: req.body.notes,
-    }); 
+  const start = new Date(req.body.start);
+  const end = new Date(req.body.end);
+  try {
+    const event = new Event({
+      user_id: req.user.id,
+      title: req.body.title,
+      location: req.body.location,
+      type: req.body.type,
+      start: start,
+      end: end,
+      repeat: req.body.repeat || "Never",
+      alert: req.body.alert || "None",
+      notes: req.body.notes,
+    });
     await event.save();
+    console.log(event);
     return res.status(200).json({ message: "Successfully added" });
-    } catch (e) {
-      console.log(e);
-      return res.status(403).json({ message: "Failed to add event. Try again." });
-    }
+  } catch (e) {
+    console.log(e);
+    return res.status(403).json({ message: "Failed to add event. Try again." });
+  }
 });
 
 //delete event
-router.get("/delete/:id", auth.authenticateToken, async (req,res) => {
-    try {
-        const event = await Event.findOneAndDelete({_id: req.params.id })
-        const events = await Event.find({user_id: req.user.id});
-        
-        return res.status(200).json({ 
-            message: "Successfully deleted",
-            events: events,
-        });
-    } catch (e) {
-        console.log(e);
-        return res.status(403).json({ message: "Failed to delete event. Try again." });
+router.get("/delete/:id", auth.authenticateToken, async (req, res) => {
+  try {
+    var eventQueried = await Event.findOne({ _id: req.params.id });
+    if ((req.user.id = eventQueried.user_id)) {
+      const event = await Event.findOneAndDelete({ _id: req.params.id });
+      return res.status(200).json({
+        message: "Successfully deleted",
+      });
+    } else {
+      return res.status(403).json({
+        message: "You are not authorized",
+      });
     }
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(403)
+      .json({ message: "Failed to delete event. Try again." });
+  }
 });
 
 //edit event
-router.post("/edit/:id", auth.authenticateToken, async(req,res) => {
-    try {
-        var event = await Event.findbyId(req.params.id);
-        var event_name= req.body.event_name;
-        if(event_name == '') {
-            event_name = event.event_name;
-        }
-        var location= req.body.location;
-        if(location == '') {
-            location = event.location;
-        }
-        var type= req.body.type;
-        if(type == '') {
-            type = event.type;
-        }
-        var start_time= req.body.start_time;
-        if(start_time == '') {
-            start_time= event.start_time;
-        }
-        var end_time= req.body.end_time;
-        if(end_time == '') {
-            end_time = event.end_time;
-        }
-        var repeat= req.body.repeat;
-        if(repeat == '') {
-            repeat= event.repeat;
-        }
-        var alert= req.body.alert;
-        if(alert == '') {
-            alert = event.alert;
-        }
-        var notes= req.body.notes;
-        if(notes == '') {
-            notes= event.notes;
-        }
-
-        var event = await Event.findOneAndUpdate( req.params.id, {
-            user_id: req.user.id,
-            event_name: event_name,
-            location: location,
-            type: type,
-            start_time: start_time,
-            end_time: end_time,
-            repeat: repeat,
-            alert: isalert,
-            notes: notes,
-        }, {new: true});
-        return res.status(200).json({ 
-            user: req.user,
-            message: "Successfully edited" });
-    } catch (e) {
-        console.log(e);
-        return res
-        .status(403)
-        .json({ 
-            message: "Failed to edit event. Try again." 
-            });
+router.post("/edit/:id", auth.authenticateToken, async (req, res) => {
+  try {
+    var event = await Event.findbyId(req.params.id);
+    var title = req.body.title;
+    if (title == "") {
+      title = event.title;
     }
+    var location = req.body.location;
+    if (location == "") {
+      location = event.location;
+    }
+    var type = req.body.type;
+    if (type == "") {
+      type = event.type;
+    }
+    var start = req.body.start;
+    if (start == "") {
+      start = event.start;
+    }
+    var end = req.body.end;
+    if (end == "") {
+      end = event.end;
+    }
+    var repeat = req.body.repeat;
+    if (repeat == "") {
+      repeat = event.repeat;
+    }
+    var alert = req.body.alert;
+    if (alert == "") {
+      alert = event.alert;
+    }
+    var notes = req.body.notes;
+    if (notes == "") {
+      notes = event.notes;
+    }
+
+    var event = await Event.findOneAndUpdate(
+      req.params.id,
+      {
+        user_id: req.user.id,
+        title: title,
+        location: location,
+        type: type,
+        start: start,
+        end: end,
+        repeat: repeat,
+        alert: isalert,
+        notes: notes,
+      },
+      { new: true }
+    );
+    return res.status(200).json({
+      user: req.user,
+      message: "Successfully edited",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(403).json({
+      message: "Failed to edit event. Try again.",
+    });
+  }
 });
 
 module.exports = router;
