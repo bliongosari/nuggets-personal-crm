61cd2205656e445c2b773135f477bbe65a5d0fa6
eputri29 <eputri@student.unimelb.edu.au>
2021-09-06 12:21:45 +1000

    add form to fill details

--- client/src/components/Contacts/AddContact.js
+++ client/src/components/Contacts/AddContact.js
@@ -5,69 +5,119 @@ function AddContact() {
 return (
     <div className="home">
 
-      <div className="searchbar">
-        <img alt="search" src="../../searchbar.svg"></img>
-        <input type="text" placeholder="SEARCH CONTACTS"></input>
+      <div className="pagetitle">
+        <h1>Add a new contact</h1>
       </div>
 
-      <div className="addcontacts">
+      {/* <div className="addcontacts">
         <button className="addbtn">
           <img alt="plus" src="../../whiteadd.svg"></img>
           <h1>ADD NEW CONTACT</h1>
         </button>
-      </div>
+      </div> */}
       
-      <div className="contactssummary">
-        <div className="summarytitle">
-          <h1>(num) Contacts</h1>
-        </div>
-        <div className="summarybutton">
-          <button className="sortbtn">
-            <h1>SORT</h1>
-            <img alt="sort" src="../../sort.svg"></img>
-        </button>
-        </div>
-      </div>
 
-      {/* Contacts Table */}
-      <div className="current-contacts">
-        <div className="current-contacts-headings">
-          <h1>Avatar</h1>
-          <h1>Contact</h1>
-          <h1>Description</h1>
+      {/* Form */}
+      <div className="contacts-form">
+        <div className="formtitle">
+          <h1>Personal Details</h1>
+          <hr></hr>
         </div>
-        <div className="current-contacts-container">
-          <img alt="events" src="../../events.svg"></img>
-          <h2>John Smith</h2>
-          <h3>Blablabla</h3>
-        </div>
-        <hr className="line"></hr>
-        <div className="current-contacts-container">
-          <img alt="events" src="../../events.svg"></img>
-          <h2>John Smith</h2>
-          <h3>Blablabla</h3>
-        </div>
-        <hr className="line"></hr>
-        <div className="current-contacts-container">
-          <img alt="events" src="../../events.svg"></img>
-          <h2>John Smith</h2>
-          <h3>Blablabla</h3>
-        </div>
-        <div className="contacts-summary-container">
-          <div className="summarytitle">
-            <h2>Page 2 of 2</h2>
-          </div>
-          <div className="navbutton">
-            <button className="backbtn">
-              <img alt="back" src="../../back.svg"></img>
-              <h1>Back</h1>
-            </button>
 
-            <button className="backbtn">
-              <h1>Next</h1>
-              <img alt="next" src="../../nextt.svg"></img>
+        <div className="profpic">
+          <img alt="plus" src="../../person-blue.svg"></img>
+        </div>
+
+        <div className="attachimage">
+          <button className="attachbtn">
+            <h1>Attach Image</h1>
+          </button>
+        </div>
+
+        <div className="details">
+          <h2>Full Name:</h2>
+          <input></input>
+        </div>
+
+        <div className="details">
+          <h2>Preferred Name:</h2>
+          <input></input>
+        </div>
+
+        <div className="detailss">
+          <h2>Birthday:</h2>
+          <div className="detailssinput">
+            <input></input>
+            <button className="formatbtn">
+              <img alt="plus" src="../../calendarr.svg"></img>
             </button>
           </div>
+          
+        </div>
+
+        <div className="details">
+          <h2>Relationship</h2>
+          <input></input>
+        </div>
+
+        <div className="detailss">
+          <h2>Tags:</h2>
+          <div className="detailssinput">
+            <input></input>
+            <button className="formatbtn">
+              <img alt="plus" src="../../palette.svg"></img>
+            </button>
+          </div>
+          
+        </div>
+
+        <div className="details">
+          <h2>How we met:</h2>
+          <input></input>
+        </div>
+
+        <div className="details">
+          <h2>Description:</h2>
+          <input></input>
+        </div>
+
+        <div className="formtitle">
+          <h1>Communication</h1>
+          <hr></hr>
+        </div>
+
+        <div className="details">
+          <h2>Email Address:</h2>
+          <input></input>
+        </div>
+
+        <div className="details">
+          <h2>Phone Number:</h2>
+          <input></input>
+        </div>
+
+        <div className="details">
+          <h2>LinkedIn:</h2>
+          <input></input>
+        </div>
+
+        <div className="details">
+          <h2>Tweeter:</h2>
+          <input></input>
+        </div>
+
+        <div className="addcontacts">
+          <button className="addbtn">
+            <img alt="plus" src="../../whiteadd.svg"></img>
+            <h1>Add Contact </h1>
+          </button>
+        </div>
+
+        <div className="addcontacts">
+          <button className="addbtn">
+            <img alt="plus" src="../../whiteadd.svg"></img>
+            <h1>Cancel</h1>
+          </button>
         </div>
       </div>
 
