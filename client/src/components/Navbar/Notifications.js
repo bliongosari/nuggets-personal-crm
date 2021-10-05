f4c0932479a56aa3ef6d86dfad69db425c7274fd
eputri29 <eputri@student.unimelb.edu.au>
2021-10-06 09:46:49 +1100

    create new all notif page

---
+++ client/src/components/Navbar/Notifications.js
@@ -0,0 +1,82 @@
+import "./Notification.css";
+import api from "../../config/axiosConfig";
+import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
+
+
+function Notification() {
+  return (
+    <>
+        <div className="mainnn-content">
+            <div className="notif-header">
+                <h1>Notifications</h1>
+            </div>
+
+
+            <div className="notiftable">
+                <div className="notiftablecontent">
+                    <IconButton aria-label="delete" size="small">
+                        <DeleteIcon fontSize="inherit"/>
+                    </IconButton>
+                    <div className="notifdesc">
+                        <h3><span>Notif Type</span></h3>
+                        <h1>Notif Title</h1>
+                        <h2>Notif Description</h2>
+                    </div>
+                    <h1>5 Oct 2019 9:35am</h1>
+                </div>
+                <hr className="notifline"></hr>
+                <div className="notiftablecontent">
+                    <IconButton aria-label="delete" size="small">
+                        <DeleteIcon fontSize="inherit"/>
+                    </IconButton>
+                    <div className="notifdesc">
+                        <h3><span>Notif Type</span></h3>
+                        <h1>Notif Title</h1>
+                        <h2>Notif Description</h2>
+                    </div>
+                    <h1>5 Oct 2019 9:35am</h1>
+                </div>
+                <hr className="notifline"></hr>
+                <div className="notiftablecontent">
+                    <IconButton aria-label="delete" size="small">
+                        <DeleteIcon fontSize="inherit"/>
+                    </IconButton>
+                    <div className="notifdesc">
+                        <h3><span>Notif Type</span></h3>
+                        <h1>Notif Title</h1>
+                        <h2>Notif Description</h2>
+                    </div>
+                    <h1>5 Oct 2019 9:35am</h1>
+                </div>
+                <hr className="notifline"></hr>
+                <div className="notiftablecontent">
+                    <IconButton aria-label="delete" size="small">
+                        <DeleteIcon fontSize="inherit"/>
+                    </IconButton>
+                    <div className="notifdesc">
+                        <h3><span>Notif Type</span></h3>
+                        <h1>Notif Title</h1>
+                        <h2>Notif Description</h2>
+                    </div>
+                    <h1>5 Oct 2019 9:35am</h1>
+                </div>
+            </div>
+           
+
+
+            {/* <div className="notiftablecontent">
+                <h2>Notif Type</h2>
+                <h2>Notif Desc</h2>
+                <h2>Thursday, 5 October 2021 &nbsp; &nbsp; &nbsp; <img alt="" src="../../close.svg"></img></h2>
+                
+            </div> */}
+            {/* <hr className="line"></hr> */}
+        </div>
+    </>
+  );
+}
+
+export default Notification;
